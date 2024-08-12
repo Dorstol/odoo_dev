d71a45b89858fd48ab47cd1a103ef0f3b5d692f5
Dorstol <olehsofronov@gmail.com>
2024-08-12 12:12:58 +0300

    fix

--- hr_hospital/models/__init__.py
+++ hr_hospital/models/__init__.py
@@ -1,1 +1,1 @@
-from . import hr_doctor, hr_disease, hr_patient, hr_patient_visit, hr_person, hr_diagnosis
+from . import hr_doctor, hr_disease, hr_patient, hr_patient_visit, hr_diagnosis
--- hr_hospital/models/hr_patient.py
+++ hr_hospital/models/hr_patient.py
@@ -2,21 +2,34 @@ from odoo import models, fields, api
 from datetime import date
 
 
+class Person(models.AbstractModel):
+    _name = "hr.person"
+    _description = "Abstract model for person"
+
+    first_name = fields.Char(
+        required=True,
+    )
+    second_name = fields.Char(
+        required=True,
+    )
+    phone_number = fields.Char()
+    photo = fields.Image()
+    gender = fields.Selection(
+        [("male", "Male"), ("female", "Female")],
+        required=True,
+        default="male",
+    )
+
+
 class Patient(models.Model):
     _inherit = "hr.person"
     _name = "hr.patient"
     _description = "Patient"
 
-    personal_doctor = fields.Many2one(
-        "hr.doctor",
-        required=True,
-    )
     date_of_birth = fields.Date(
         required=True,
     )
-    passport_data = fields.Char(
-        required=True,
-    )
+    passport_data = fields.Char()
     contact_person = fields.Char()
     age = fields.Integer(
         compute="_compute_age",
@@ -28,10 +41,28 @@ class Patient(models.Model):
     doctor_id = fields.Many2one(
         "hr.doctor",
     )
+    doctor_name = fields.Char(
+        related="doctor_id.full_name",
+        store=True,
+    )
     visit_ids = fields.One2many(
         "hr.patient.visit",
         "patient_id",
     )
+    patient_name = fields.Char(
+        compute="_compute_patient_name",
+        store=True,
+    )
+
+    @api.depends("first_name", "second_name")
+    def _compute_patient_name(self):
+        for record in self:
+            if record.first_name and record.second_name:
+                record.patient_name = f"{record.first_name} {record.second_name}"
+            elif record.first_name:
+                record.patient_name = record.first_name
+            else:
+                record.patient_name = record.second_name
 
     @api.depends("date_of_birth")
     def _compute_age(self):
